d619155df861eace4edc577832245079b9d12c7b
Hugo Fuhlendorff <hugo@neowit.io>
2024-02-27 13:14:38 +0100

    testing

---
+++ api/api.go
@@ -0,0 +1,7 @@
+package api
+
+import "fmt"
+
+func StartServer() {
+	fmt.Println("Starting server")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/tool4gh/api"
+
+func main() {
+	api.StartServer()
+}
